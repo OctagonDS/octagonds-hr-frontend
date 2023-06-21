36208ba13c25fddee8a088513a7c070cdcad0116
Madvender <accountas@mail.ru>
2023-06-21 10:17:59 +0400

    content item

--- src/page/content.jsx
+++ src/page/content.jsx
@@ -1,7 +1,55 @@
+import { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 const Content = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 })
+    return () => {}
+  }, [])
+
   return (
     <div className="footer-block">
-      <div className="slide-block"></div>
+      <div className="content_block">
+        <div className="content" data-aos="fade-up">
+          <div data-aos="fade-up" className="content__title">
+            этому городу ножен новый{' '}
+            <span className="title_back_color">герой</span>
+          </div>
+          <div className="content__desc">
+            <p>
+              Lorem Ipsum - это текст-"рыба", часто используемый в печати и
+              вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов
+              на латинице с начала XVI века. В то время некий безымянный
+              печатник создал большую коллекцию размеров и форм шрифтов,
+              используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не
+              только успешно пережил без заметных изменений пять веков, но и
+              перешагнул в электронный дизайн. Его популяризации в новое время
+              послужили публикация листов Letraset с образцами Lorem Ipsum в
+              60-х годах и, в более недавнее время, программы электронной
+              вёрстки типа Aldus PageMaker, в шаблонах которых используется
+              Lorem Ipsum.
+            </p>
+          </div>
+        </div>
+        <div className="content" data-aos="fade-up">
+          <div data-aos="fade-up" className="content__title">
+            вакансии
+          </div>
+        </div>
+        <div className="content" data-aos="fade-up">
+          <div data-aos="fade-up" className="content__item__row">
+            <div className="content__bit"></div>
+            <div className="content__item"></div>
+          </div>
+        </div>
+        <div className="content" data-aos="fade-up">
+          <div data-aos="fade-up" className="content__item__row">
+            <div className="content__item"></div>
+            <div className="content__bit"></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
