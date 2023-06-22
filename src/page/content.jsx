00c9dba8afe857cf2d4fe1b60d633feb493ab8ef
Madvender <accountas@mail.ru>
2023-06-22 07:35:56 +0400

    Rick

--- src/page/content.jsx
+++ src/page/content.jsx
@@ -1,47 +1,83 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 const Content = () => {
+  const [portal, setPortal] = useState('')
+
   useEffect(() => {
     AOS.init({ duration: 1000 })
     return () => {}
   }, [])
 
+  console.log(portal)
+
   return (
-    <div className="footer-block">
-      <div className="content_block">
-        <div className="content">
-          <div className="content__title">
-            этому городу ножен новый{' '}
-            <span className="title_back_color">герой</span>
+    <div className="content_block">
+      <div className="content">
+        <div className="content__title">
+          этому городу нужен новый{' '}
+          <span className="title_back_color">герой</span>
+        </div>
+        <div className="content__desc">
+          Lorem Ipsum - это текст-"рыба", часто используемый в печати и
+          вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на
+          латинице с начала XVI века. В то время некий безымянный печатник
+          создал большую коллекцию размеров и форм шрифтов, используя Lorem
+          Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил
+          без заметных изменений пять веков, но и перешагнул в электронный
+          дизайн. Его популяризации в новое время послужили публикация листов
+          Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее
+          время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах
+          которых используется Lorem Ipsum.
+        </div>
+      </div>
+      <div className="content" style={{ marginBottom: 20 }}>
+        <div className="content__title" style={{ marginBottom: 20 }}>
+          наши вакансии
+        </div>
+      </div>
+      <div className="content" data-aos="fade-up">
+        <div
+          className="content__item__row"
+          onMouseOver={() => setPortal('-open')}
+          onMouseOut={() => setPortal('-close')}
+        >
+          <div className="content__bit">
+            <div className={`portal__box${portal}`}>
+              <div className="portal" />
+            </div>
+            <div className="rick" />
+            {portal && (
+              <div
+                className="bubble bottom rick__dialog"
+                style={{ width: 233 }}
+              >
+                <div className="content__bit__text">
+                  <div>Погнали!</div> Приключение на 5 минут.
+                </div>
+              </div>
+            )}
           </div>
-          <div className="content__desc">
-            Lorem Ipsum - это текст-"рыба", часто используемый в печати и
-            вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на
-            латинице с начала XVI века. В то время некий безымянный печатник
-            создал большую коллекцию размеров и форм шрифтов, используя Lorem
-            Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил
-            без заметных изменений пять веков, но и перешагнул в электронный
-            дизайн. Его популяризации в новое время послужили публикация листов
-            Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее
-            время, программы электронной вёрстки типа Aldus PageMaker, в
-            шаблонах которых используется Lorem Ipsum.
+          <div className="content__item">
+            <h4>Python разработчик</h4>
+            <p>
+              Lorem Ipsum - это текст-"рыба", часто используемый в печати и
+              вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов
+              на латинице с начала XVI века. В то время некий безымянный
+              печатник создал большую коллекцию размеров и форм шрифтов,
+              используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не
+              только успешно пережил без заметных изменений пять веков, но и
+              перешагнул в электронный дизайн.
+            </p>
           </div>
         </div>
-        <div className="content">
-          <div className="content__title">вакансии</div>
-        </div>
-        <div className="content" data-aos="fade-up">
-          <div className="content__item__row">
-            <div className="content__bit"></div>
-            <div className="content__item"></div>
-          </div>
-        </div>
-        <div className="content" data-aos="fade-up">
-          <div className="content__item__row">
-            <div className="content__item"></div>
-            <div className="content__bit"></div>
+      </div>
+      <div className="content" data-aos="fade-up">
+        <div className="content__item__row">
+          <div className="content__item"></div>
+          <div className="content__bit">
+            <div className="knight" />
           </div>
         </div>
       </div>
