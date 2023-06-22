fb42633ae9d5bdebc24fb255f475cc847a896262
Madvender <accountas@mail.ru>
2023-06-22 12:41:04 +0400

    Rick Title Fix

--- src/page/content.jsx
+++ src/page/content.jsx
@@ -83,7 +83,60 @@ const Content = () => {
       </div>
       <div className="content" data-aos="fade-up">
         <div className="content__item__row">
-          <div className="content__item"></div>
+          <div className="content__item">
+            <h4>HR</h4>
+            <p>
+              Lorem Ipsum - это текст-"рыба", часто используемый в печати и
+              вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов
+              на латинице с начала XVI века. В то время некий безымянный
+              печатник создал большую коллекцию размеров и форм шрифтов,
+              используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не
+              только успешно пережил без заметных изменений пять веков, но и
+              перешагнул в электронный дизайн.
+            </p>
+          </div>
+          <div className="content__bit">
+            <div className="knight" />
+          </div>
+        </div>
+      </div>
+      <div className="content" data-aos="fade-up">
+        <div
+          className="content__item__row"
+          onMouseOver={() => setPortal('-open')}
+          onMouseOut={() => setPortal('-close')}
+        >
+          <div className="content__bit">
+            <div className="glados" />
+          </div>
+          <div className="content__item">
+            <h4>Тестировщик</h4>
+            <p>
+              Lorem Ipsum - это текст-"рыба", часто используемый в печати и
+              вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов
+              на латинице с начала XVI века. В то время некий безымянный
+              печатник создал большую коллекцию размеров и форм шрифтов,
+              используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не
+              только успешно пережил без заметных изменений пять веков, но и
+              перешагнул в электронный дизайн.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="content" data-aos="fade-up">
+        <div className="content__item__row">
+          <div className="content__item">
+            <h4>Team Lead</h4>
+            <p>
+              Lorem Ipsum - это текст-"рыба", часто используемый в печати и
+              вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов
+              на латинице с начала XVI века. В то время некий безымянный
+              печатник создал большую коллекцию размеров и форм шрифтов,
+              используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не
+              только успешно пережил без заметных изменений пять веков, но и
+              перешагнул в электронный дизайн.
+            </p>
+          </div>
           <div className="content__bit">
             <div className="knight" />
           </div>
